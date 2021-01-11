ce279f165f379c789cc232a4e50c0abf01cefdec
Alin Badila <badila.alin@yahoo.com>
2021-01-11 18:30:23 +0200

    tentacles test

--- src/netdava/git_stars_counter.clj
+++ src/netdava/git_stars_counter.clj
@@ -1,7 +1,16 @@
 (ns netdava.git-stars-counter
+  (:require [tentacles/tentacles.repos :as repos])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  ([]
+   (println "Hello, World!"))
+  ([username] 
+   (println "Hello" username)))
+
+(comment
+  (-main)
+  (-main "alinbadila")
+  (repos/user-repos "alinbadila")
+  )
