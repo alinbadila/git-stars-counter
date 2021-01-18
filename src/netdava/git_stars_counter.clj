aaea30e976f0d327cdf7b778a64d14710647cbdc
Alin Badila <badila.alin@yahoo.com>
2021-01-18 17:00:43 -0500

    adaugat autentificare folosind environ

--- src/netdava/git_stars_counter.clj
+++ src/netdava/git_stars_counter.clj
@@ -1,16 +1,24 @@
 (ns netdava.git-stars-counter
-  (:require [tentacles/tentacles :as repos])
+  (:require [tentacles.users :as tu]
+            [tentacles.core :as tc]
+            [environ.core :refer [env]])
   (:gen-class))
+
+(defn get-stars 
+  []
+  (tc/api-call :get "user/starred" nil {:auth (tc/environ-auth env)}))
 
 (defn -main
   "I don't do a whole lot ... yet."
-  ([]
-   (println "Hello, World!"))
-  ([username] 
-   (println "Hello" username)))
+  [&args]
+  (get-stars))
+  
 
 (comment
-  (-main)
-  (-main "alinbadila")
-  (repos/user-repos "alinbadila")
+  ;; (System/getProperty "user.home")
+  ;; (get-stars "alinbadila" "test")
+  ;; (println env)
+  (tc/environ-auth env)
+  (get-stars)
+  (tu/user {:auth (tc/environ-auth env)})
   )
